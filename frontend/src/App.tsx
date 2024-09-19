2c19522546831b3cfededd4a83a0a560e7fd1a4d
Shayan Halder <halders2@uci.edu>
2024-09-19 09:43:05 -0700

    add requirements.txt and .env variables

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,30 @@
 import { useRef, useState } from "react"
 import './App.css'
+ 
+const ENDPOINT = import.meta.env.VITE_BACKEND 
 
-const ENDPOINT = 'http://127.0.0.1:5000/parse-file'
 function App() {
-    const inputRef = useRef<HTMLInputElement>(null)
+    const inputRef = useRef<HTMLInputElement>(null);
     const [outputText, setOutputText] = useState<string>("");
+    const [loading, setLoading] = useState<boolean>(false);
 
     async function sendFile() { 
-        let formData = new FormData()
+        setLoading(true);
+        let formData = new FormData();
         if (inputRef.current && inputRef.current.files) {
             const file = inputRef.current.files[0]
             formData.append('file', file) 
-            let response = await fetch(ENDPOINT, {
+            let response = await fetch(`${ENDPOINT}/parse-file`, {
               method: "POST",
               body: formData, 
               headers: {
                 'Accept': 'application/json'
               }, 
-            }) 
-            const data = await response.json() 
+            }); 
+            const data = await response.json(); 
             const outputText = data.output;
             setOutputText(outputText);
-
-
+            setLoading(false);
         }
     }
 
@@ -33,11 +35,8 @@ function App() {
                 <div>Upload input file here:  </div>
                 <div className="file-container"><input type='file' id='imageUpload' name='imageUpload' accept='*' ref={inputRef}/> </div>
                 <div><button type='submit' onClick={sendFile}> Submit </button></div>
-                <textarea value={outputText} placeholder="Bash script output here..." readOnly>
-
-                </textarea>
+                <textarea value={outputText} placeholder={loading ? "Loading..." : "Bash script output here..."} readOnly></textarea>
             </div>
-
         </div>
     )
 }
